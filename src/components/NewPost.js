16b539da564e918d44179510270920ba84cb1319
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-02-27 13:39:00 +0100

    Add new post outside

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -3,20 +3,15 @@ import "../styles/NewPost.css";
 export const NewPostComponent = () => {
   return (
     <div className="newPost">
+      <h4 className="newPost__title">Add new post</h4>
       <form>
-        <h4>Add new post</h4>
         <div className="newPost__control">
           <label for="title">Title:</label>
           <input id="title" type="text" />
         </div>
         <div className="newPost__control">
           <label for="description">Description:</label>
-          <textarea
-            id="description"
-            // value={this.state.textAreaValue}
-            // onChange={this.handleChange}
-            rows={2}
-          />
+          <textarea id="description" rows={2} />
         </div>
       </form>
     </div>
