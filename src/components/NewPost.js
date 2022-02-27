2b8b299b87783aedba7eae2740c429cdb7bf2e3d
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-02-27 13:36:16 +0100

    add new post css

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -4,9 +4,19 @@ export const NewPostComponent = () => {
   return (
     <div className="newPost">
       <form>
+        <h4>Add new post</h4>
         <div className="newPost__control">
-          <label>Title</label>
-          <input type="text" />
+          <label for="title">Title:</label>
+          <input id="title" type="text" />
+        </div>
+        <div className="newPost__control">
+          <label for="description">Description:</label>
+          <textarea
+            id="description"
+            // value={this.state.textAreaValue}
+            // onChange={this.handleChange}
+            rows={2}
+          />
         </div>
       </form>
     </div>
