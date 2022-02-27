97f37af99f05019ee64e5e305611ed4b7db4bc9e
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-02-27 17:41:41 +0100

    add new post simply

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,22 +1,49 @@
+import { useState } from "react";
 import "../styles/NewPost.css";
+import { PostComponent } from "./Post";
 
 export const NewPostComponent = () => {
+  const [titleValue, setTitleValue] = useState();
+  const [descriptionValue, setDescriptionValue] = useState();
+  const [newPost, setNewPost] = useState({});
+  const handleTitleChange = (event) => {
+    setTitleValue(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescriptionValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+      title: titleValue,
+      description: descriptionValue,
+    };
+    setNewPost(newPost);
+  };
+
   return (
     <div className="newPost">
       <h4 className="newPost__title">Add new post</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="newPost__control">
           <label for="title">Title:</label>
-          <input id="title" type="text" />
+          <input id="title" type="text" onChange={handleTitleChange} />
         </div>
         <div className="newPost__control">
           <label for="description">Description:</label>
-          <textarea id="description" rows={2} />
+          <textarea
+            id="description"
+            rows={2}
+            onChange={handleDescriptionChange}
+          />
         </div>
         <div className="newPost__actions">
           <button type="submit">Add post</button>
         </div>
       </form>
+      <PostComponent title={newPost.title} description={newPost.description} />
     </div>
   );
 };
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,12 @@
-import { useState } from "react";
 import "../styles/Post.css";
 
 export const PostComponent = (props) => {
-  const [title, setTitle] = useState("ReactJS lesson");
-  const handleClick = () => {
-    setTitle("Updated !!");
-  };
+  if (!props.title) return null;
   return (
     <div className="UserPost">
       <div className="UserPost__content">
-        <h3 className="UserPost__Title">{title}</h3>
-        <p className="UserPost__Description">
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-        </p>
-        <button onClick={handleClick}>Change Title</button>
+        <h3 className="UserPost__Title">{props.title}</h3>
+        <p className="UserPost__Description">{props.description}</p>
       </div>
     </div>
   );
